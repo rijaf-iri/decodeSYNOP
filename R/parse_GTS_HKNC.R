31faf8a8a91b949c3164767c2745d48fa5d3ae69
Rija Faniriantsoa <rijaf@iri.columbia.edu>
2022-03-10 20:10:46 +0300

    update parsing GTS bulletins

--- R/parse_GTS_HKNC.R
+++ R/parse_GTS_HKNC.R
@@ -17,22 +17,37 @@
 #' @export
 
 parse_GTS_Bulletin <- function(bulletin_file){
-    rawData <- readLines(bulletin_file, skipNul = TRUE, warn = FALSE)
+    rawData <- readLines(bulletin_file, warn = FALSE)
     rawData <- trimws(rawData)
     rawData <- rawData[rawData != ""]
 
-    istart <- grep("AAXX", rawData)
-    if(length(istart) == 0) return(NULL)
+    ## remove on windows?
+    rawData <- gsub("\\x*", "", rawData)
 
-    iend <- c(istart[-1] - 1, length(rawData))
-    rawData <- lapply(seq_along(istart), function(j){
-        rawData[istart[j]:iend[j]]
+    iblk1 <- grep("\\\001", rawData)
+    if(length(iblk1) == 0) return(NULL)
+    iblk2 <- c(iblk1[-1] - 1, length(rawData))
+    rawData <- lapply(seq_along(iblk1), function(j){
+        rawData[iblk1[j]:iblk2[j]]
     })
+    ix <- sapply(rawData, function(x){
+        ii <- grep("AAXX", x)
+        if(length(ii > 0)) TRUE else FALSE
+    })
+    if(!any(ix)) return(NULL)
+
+    rawData <- rawData[ix]
+
     rawData <- lapply(rawData, function(x){
+        ix <- grep("^AAXX", x)
+        x <- x[ix:length(x)]
         s <- x[1]
         y <- x[-1]
         ie <- grep("=", y)
-        y <- y[-((ie[length(ie)] + 1):length(y))]
+        iq1 <- ie[length(ie)] + 1
+        if(iq1 < length(y))
+            y <- y[-(iq1:length(y))]
+
         is <- c(1, ie[-length(ie)] + 1)
         y <- lapply(seq_along(is), function(j){
             paste0(y[is[j]:ie[j]], collapse = " ")
@@ -42,6 +57,8 @@ parse_GTS_Bulletin <- function(bulletin_file){
     })
 
     rawData <- do.call(c, rawData)
+    ix <- grep("callto|N I L", rawData)
+    if(length(ix) > 0) rawData <- rawData[-ix]
 
     return(rawData)
 }
